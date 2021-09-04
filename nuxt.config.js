471f37c449d64fcb8e6c7165802c09323b78885b
gurumee <gurumee92@gmail.com>
2021-09-04 18:15:14 +0900

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,14 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'learn-nuxtjs',
+    title: 'Nuxt Shopping',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Shopping App with Nuxt.js' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
